44e4e235fec894887a3282b8e91a947898b8ddf6
tolulope-kresha <akinbinutoluwalope@gmail.com>
2022-05-27 12:55:01 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,39 @@
+# Read text from a file, and count the occurence of words in that text
+# Example:
+# count_words("The cake is done. It is a big cake!") 
+# --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
+
+import string
+
+
+def read_file_content(filename):
+    # [assignment] Add your code here 
+    with open(filename) as f:
+        file = f.read()
+    
+    return file
+print(read_file_content('./story.txt'))
+
+
+def count_words():
+    text = read_file_content("./story.txt")
+    # [assignment] Add your code here
+    # removing punctuation marks
+    text = text.replace(',', "")
+    text = text.replace('.', '')
+    text = text.replace('?','')
+    
+    counts = dict()
+    words = text.lower()
+    words = words.split()
+   
+    for word in words:
+        if (word in counts.keys()):
+            counts[word] +=1
+        else:
+            counts[word] =1
+    return counts
+
+print(count_words())
+
+    # return {"as": 10, "would": 20}
